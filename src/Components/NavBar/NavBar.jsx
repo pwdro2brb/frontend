1c76357cbf6f596d7803045cb966b55507dac246
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2024-10-30 20:46:53 -0300

    Adicionando barra inicial da loja Melhorando os estilos mna barra

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,12 @@
-import React from 'react' 
+import React, { useState } from 'react' 
 import './NavBar.css'
 import logo from 'C:/Users/User/Desktop/loja_digital/frontend/src/Assets/logo.png'
 import cart_icon from 'C:/Users/User/Desktop/loja_digital/frontend/src/Assets/cart_icon.png'
 
 const NavBar = () => {
+
+    const [menu,setMenu] = useState("Comprar");
+
     return(
         <div className="navbar">
             <div className="nav-logo">
@@ -11,14 +14,15 @@ const NavBar = () => {
                 <p>SHOPPER</p>
             </div>
             <ul className="nav-menu">
-                <li>Comprar</li>
-                <li>Homens</li>
-                <li>Mulheres</li>
-                <li>Crianças</li>
+                <li onClick={()=>{setMenu("Comprar")}}>Comprar{menu==="Comprar"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("Homens")}}>Homens{menu==="Homens"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("Mulheres")}}>Mulheres{menu==="Mulheres"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("Crianças")}}>Crianças{menu==="Crianças"?<hr/>:<></>}</li>
             </ul>
             <div className="nav-login-cart">
                 <button>Login</button>
                 <img src={cart_icon} alt="" class="princ2"/>
+                <div className="nav-cart-count">0</div>
             </div>
         </div>
     )
