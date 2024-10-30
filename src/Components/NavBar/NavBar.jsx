90cd9fbc5f17ec75525e70414df0ce9e73e7d075
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2024-10-29 21:01:32 -0300

    Adicionando barra inicial da loja

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,25 @@
 import React from 'react' 
 import './NavBar.css'
+import logo from 'C:/Users/User/Desktop/loja_digital/frontend/src/Assets/logo.png'
+import cart_icon from 'C:/Users/User/Desktop/loja_digital/frontend/src/Assets/cart_icon.png'
 
 const NavBar = () => {
     return(
-        <div class="navbar">
-
+        <div className="navbar">
+            <div className="nav-logo">
+                <img src={logo} alt='' class="princ1"/>
+                <p>SHOPPER</p>
+            </div>
+            <ul className="nav-menu">
+                <li>Comprar</li>
+                <li>Homens</li>
+                <li>Mulheres</li>
+                <li>Crianças</li>
+            </ul>
+            <div className="nav-login-cart">
+                <button>Login</button>
+                <img src={cart_icon} alt="" class="princ2"/>
+            </div>
         </div>
     )
 }
